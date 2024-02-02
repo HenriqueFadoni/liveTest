b3822f6c34cfa4c4f51063125d003bd64d3ef6a6
Henrique Fadoni <henriquefadonii@gmail.com>
2024-02-02 14:17:23 -0300

    feat(styles): reusable

---
+++ styles/variables.tsx
@@ -0,0 +1,15 @@
+const blackColor = '#000000';
+const whiteColor = '#ffffff';
+const whiteDarkColor = '#DBEFF4';
+const greyColor = 'grey';
+const blueDarkColor = '#3B5E65';
+const blueLightColor = '#8AC9D6';
+
+export default {
+	blackColor,
+	whiteColor,
+	whiteDarkColor,
+	greyColor,
+	blueDarkColor,
+	blueLightColor,
+}
